e46fba3524ffaf71e25f71f71f38e14ae791cf03
Ashish Ranjan <ashish061123@gmail.com>
2026-07-06 16:01:45 +0530

    fixing random bugs

--- backend/DBQuery.py
+++ backend/DBQuery.py
@@ -1,12 +1,12 @@
 import psycopg2
-
+import os
 
 #enter these field with the same data that you used for running postgres container
-host = ""
-user = ""
-password = ""
-database = ""
-port = 0
+host = os.getenv("DB_HOST")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+database = os.getenv("DB_NAME")
+port = os.getenv("DB_PORT")
 
 
 
